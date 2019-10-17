98eca4c30409a7b8ad889dbc50328aeea6963ac5
BaileyBrennanUoE <bbrennan04@gmail.com>
2019-10-17 13:50:51 +0100

    malformedUrlException, work in progress cw2

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,13 +1,20 @@
 package uk.ac.ed.inf.powergrab;
+import java.net.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String mapString = "http://homepages.inf.ed.ac.uk/stg/powergrab/" + args[2] + "/" + 
+                args[1] + "/" + args[0] + "/powergrabmap.geojson";
+        geoMap map = new geoMap(new URL(mapString));
+        Position startPoint = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+        Integer psuedoSeed = Integer.parseInt(args[5]);
+        String droneType = args[6];
+        Game powerGrab = new Game(droneType, startPoint);
+        
+        powerGrab.playGame();
     }
+    
+    
 }
