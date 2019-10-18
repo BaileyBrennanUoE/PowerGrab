0c1f01469cd92c030bdb8153da24e22c29f689a5
BaileyBrennanUoE <bbrennan04@gmail.com>
2019-10-18 12:17:09 +0100

    Added dependencies, implemented map fetching and a few basic drone and game operations. Working on station interactions.

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,20 +1,38 @@
 package uk.ac.ed.inf.powergrab;
 import java.net.*;
+import java.io.*;
+import com.mapbox.geojson.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException{
+        
         String mapString = "http://homepages.inf.ed.ac.uk/stg/powergrab/" + args[2] + "/" + 
                 args[1] + "/" + args[0] + "/powergrabmap.geojson";
-        geoMap map = new geoMap(new URL(mapString));
-        Position startPoint = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-        Integer psuedoSeed = Integer.parseInt(args[5]);
+        URL mapURL = new URL(mapString);    //creates URL from where the map will be downloaded from 
+        Position startPoint = new Position(Double.parseDouble(args[3]), Double.parseDouble(args[4]));   //start point of the drone
+        Integer pseudoSeed = Integer.parseInt(args[5]);     //seed for the pseudo-random generator
         String droneType = args[6];
-        Game powerGrab = new Game(droneType, startPoint);
         
+        URLConnection urlConn = mapURL.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) urlConn;
+        conn.setReadTimeout(10000); // milliseconds
+        conn.setConnectTimeout(15000); // milliseconds
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        conn.connect(); 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder mapSource = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            mapSource.append(line);
+        }
+        FeatureCollection fc = FeatureCollection.fromJson(mapSource.toString());
+        
+        Game powerGrab = new Game(droneType, startPoint, pseudoSeed, fc);
         powerGrab.playGame();
     }
+    
     
     
 }
